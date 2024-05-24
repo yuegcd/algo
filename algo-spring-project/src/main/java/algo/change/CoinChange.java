36f9f24e022651ff08a0081f4288e9d229f332e9
yueshigao <yueshi.gao@gmail.com>
2024-05-24 15:39:36 +0200

    Change(322.Coin Change): Use recursive call

--- algo-spring-project/src/main/java/algo/change/CoinChange.java
+++ algo-spring-project/src/main/java/algo/change/CoinChange.java
@@ -26,15 +26,41 @@ public class CoinChange {
 
     public List<Integer> change(int amount) {
         List<Integer> result = new ArrayList<>(Arrays.asList(0, 0, 0, 0));
-        for (int i = denominations.size() - 1; i >= 0; i--) {
-            int rest = amount % denominations.get(i);
-            if (i == denominations.size() - 1) {
-                result.set(i, amount / denominations.get(i));
-            } else {
-                result.set(i, amount % denominations.get(i + 1) / denominations.get(i));
-            }
-            if (rest == 0) break;
-        }
+        change(amount, denominations.size() - 1, result);
         return result;
     }
+
+    private void change(int amount, int i, List<Integer> result) {
+        if (i < 0) return;
+        int numberOfCoins = amount / denominations.get(i);
+        result.set(i, numberOfCoins);
+        int reminder = amount - numberOfCoins * denominations.get(i);
+        change(reminder, i - 1, result);
+    }
 }
+
+/*
+-- Time Complexity
+- Outer change Method:
+The outer change method initializes a list and calls the helper method change.
+This initialization takes constant time, O(1), as the list size is fixed at 4.
+
+- Recursive change Method:
+The change method is recursively called once for each denomination.
+Let's assume denominations has n elements.
+The recursive method makes n calls, decrementing i each time until it reaches -1.
+Each call performs constant-time operations: calculating the number of coins, updating the list, and computing the remainder.
+Thus, the time complexity of the recursive method is O(n).
+Therefore, the total time complexity is O(n), where n is the number of denominations.
+
+-- Space Complexity
+- Outer change Method:
+The outer method initializes a list of size 4, which is O(1) space complexity.
+- Recursive change Method:
+The recursion depth is n because there is one recursive call for each denomination.
+Each recursive call adds a frame to the call stack, so the space required for the call stack is O(n).
+
+-- Auxiliary Space:
+The result list is passed by reference, and its size does not change, so it takes O(1) space.
+Thus, the overall space complexity is O(n) due to the call stack.
+*/
